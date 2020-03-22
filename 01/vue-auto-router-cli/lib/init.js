2f204d6422c7ed0543cf293f078f01903083271c
zhengyh001 <zhengyh001@cmft.com>
2020-03-22 18:25:45 +0800

    01-node第一天

--- 01/vue-auto-router-cli/lib/init.js
+++ 01/vue-auto-router-cli/lib/init.js
@@ -4,9 +4,10 @@ const figlet = promisify(require('figlet'))
 
 const clear = require('clear')
 const chalk = require('chalk') // 打印的日志的粉笔库，颜色
+const open = require('open')
 
 // 定一个log方法，所有日志都要粉笔一下
-const log = content => console.log(chalk.green(content))
+const log = content => console.log(chalk.blue(content))
 const { clone } = require('./download')
 
 module.exports = async name => {
@@ -18,17 +19,24 @@ module.exports = async name => {
   clone('github:su37josephxia/vue-template', name)
   // 安装依赖
   log('安装依赖')
+  log(process.platform)
+  await spawn('npm.cmd', ['install'],{cwd: `./${name}`})
+  log(`
+  =========
+    依赖安装成功
+  =========
+  `)
+  // 打开浏览器
+  open('http://localhost:8080')
+  await spawn('npm.cmd', ['run','serve'], {cwd: `/${name}`})
 }
-/**
- * 子进程安装依赖并打印日志
- * @param  {...any} args
- */
+
 // 安装依赖 spwan子进程安装依赖不会输出依赖，所以要写方法对接，日志是流的方式
-function spwan(...args) {
+function spawn(...args) {
   // 子进程库，用来安装依赖
-  const { spwan } = require('child_process')
+  const { spawn } = require('child_process')
   return new Promise(resolve => {
-    const childProc = spwan(...args)
+    const childProc = spawn(...args)
     childProc.stdout.pipe(process.stdout)
     childProc.stderr.pipe(process.stderr)
     // 子进程完成的事件监听
@@ -37,3 +45,4 @@ function spwan(...args) {
     })
   })
 }
+
